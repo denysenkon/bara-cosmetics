a371365516f2f329bacbf1c025d73e813f983cbf
denysenkon <natalia.denysenko@mgid.com>
2020-10-25 20:54:37 +0000

    added disclaimer and privacy popups

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 
+// termini
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -25,3 +26,58 @@ window.onclick = function(event) {
   }
 }
 
+// informativa sulla privacy
+
+
+var modal = document.getElementById("myModalPrivacy");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtnPrivacy");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Disclaimer 
+
+var modal = document.getElementById("myModalDisclaimer");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtnDisclaimer");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
