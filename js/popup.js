eba136a779655e4cf17ddca23ed651f827e764a0
denysenkon <natalia.denysenko@mgid.com>
2020-10-26 10:04:12 +0000

    positioned sticky button

--- js/popup.js
+++ js/popup.js
@@ -1,66 +1,66 @@
 
-// termini
-// Get the modal
-var modal = document.getElementById("myModal");
+// Disclaimer 
+
+var modalDisclaimer = document.getElementById("myModalDisclaimer");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btnDisclaimer = document.getElementById("myBtnDisclaimer");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanDisclaimer = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btnDisclaimer.onclick = function() {
+  modalDisclaimer.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanDisclaimer.onclick = function() {
+  modalDisclaimer.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function(eventDisclaimer) {
+  if (eventDisclaimer.target == modalDisclaimer) {
+    modalDisclaimer.style.display = "none";
   }
 }
 
 // informativa sulla privacy
 
 
-var modal = document.getElementById("myModalPrivacy");
+var modalPrivacy = document.getElementById("myModalPrivacy");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtnPrivacy");
+var btnPrivacy = document.getElementById("myBtnPrivacy");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanPrivacy = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btnPrivacy.onclick = function() {
+  modalPrivacy.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanPrivacy.onclick = function() {
+  modalPrivacy.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function(eventPrivacy) {
+  if (eventPrivacy.target == modalPrivacy) {
+    modalPrivacy.style.display = "none";
   }
 }
 
 
-// Disclaimer 
-
-var modal = document.getElementById("myModalDisclaimer");
+// termini
+// Get the modal
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtnDisclaimer");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
